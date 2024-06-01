5ca7289fe1f236a3de0c727bfa4f93f3a545a583
Royas Shakya <shkroyas@gmail.com>
2024-06-01 18:25:46 +0545

    Learned about variable and datatypes in javascript

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,13 +1,42 @@
-alert("Hello World");
+console.log("Hey this is Day 1");
 
-console.log("Code is running...")
-console.log("Code is also running...")
-console.log("Code is looking like a wow...")
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "Karan";
+let _a = "Shubham";
+// var 1_a = "Rohan"; // Not Allowed
 
-var a = prompt("Enter your number")
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    // let a = 66;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
 
-console.log("Your number is " + a)
 
-document.title = "Hey I am good"
+let x = "Hari";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
 
-// document.body.style.backgroundColor = "red"
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Harry",
+    "job code": 5600,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
