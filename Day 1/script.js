06d7b976c3a489ef43f5ba87c5f929ad576c6726
Royas Shakya <shkroyas@gmail.com>
2024-06-01 18:19:51 +0545

    Learnt Basics of Javascript(Frontend+Backend)

---
+++ Day 1/script.js
@@ -0,0 +1,13 @@
+alert("Hello World");
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+
+console.log("Your number is " + a)
+
+document.title = "Hey I am good"
+
+// document.body.style.backgroundColor = "red"
