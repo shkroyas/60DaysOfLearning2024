e8ee32d0f1c3ce6dfd378ba45e8fa2c4d051f339
Royas Shakya <shkroyas@gmail.com>
2024-06-02 22:16:42 +0545

    Learned JavaScript Conditionals, Ternary Operator.

---
+++ Day 2/index.js
@@ -0,0 +1,26 @@
+console.log("Javascript conditionals")
+
+let age = 1;
+
+if (age == 18) {
+    console.log("You can drive");
+}
+
+else if (age == 0) {
+    console.log("Are you kidding?")
+}
+
+else if (age == 1) {
+    console.log("Are you again kidding?")
+}
+
+else {
+    console.log("You cannot drive");
+}
+
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);//ternary operator
+
+console.log(c);// 2
+
